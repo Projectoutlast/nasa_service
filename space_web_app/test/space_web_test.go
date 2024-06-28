7a3cb54072531875e2458d25d085ed4fe86cea2c
Viktor Salimov <viktorsalimov544@gmail.com>
2024-06-28 13:19:58 +0300

    - stable tests;

--- space_web_app/test/space_web_test.go
+++ space_web_app/test/space_web_test.go
@@ -12,39 +12,30 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMain(t *testing.T) {
-	_ = logging.New("testing", os.Stdout)
-	_ = logging.New("production", os.Stdout)
+func TestSuccessStartSpaceWebServer(t *testing.T) {
+	t.Parallel()
 
-	// Preparation before test
 	logger, cfg, router := preparationStartSpaceWebServer(t)
 
-	// Testing error start http server
-	cfg.Server.Port = -4
-	errorStartApplication := app.New(logger, cfg, router)
-	require.Error(t, errorStartApplication.HTTPServer.Run())
-
-	// // Testing panic in start http server
-	defer func() {
-		if r := recover(); r == nil {
-			t.Errorf("expected a panic")
-		}
-	}()
-
-	errorStartApplication.HTTPServer.MustRun()
-
-	// Testing normal start http server
-	cfg.Server.Port = 50061
 	successStartApplication := app.New(logger, cfg, router)
-	go successStartApplication.HTTPServer.MustRun()
 
 	var err error
+
 	go func() {
 		err = successStartApplication.HTTPServer.Run()
 	}()
 
 	require.Nil(t, err)
+}
 
+func TestCreateLogger(t *testing.T) {
+	t.Parallel()
+
+	logger := logging.New("testing", os.Stdout)
+	require.NotNil(t, logger)
+
+	logger = logging.New("production", os.Stdout)
+	require.NotNil(t, logger)
 }
 
 func preparationStartSpaceWebServer(_ *testing.T) (*slog.Logger, *config.Config, *mux.Router) {
